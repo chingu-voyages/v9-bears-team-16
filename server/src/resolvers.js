19ab24eca5726bbc96f18d2694650da7114f2999
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-06-07 14:20:05 -0700

    remove custom resolvers

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,36 +1,36 @@
 module.exports.resolvers = {
   Query: {
-    feed: (parent, args, context) => {
-      return context.prisma.posts({ where: { published: true } })
-    },
-    drafts: (parent, args, context) => {
-      return context.prisma.posts({ where: { published: false } })
-    },
-    post: (parent, { id }, context) => {
-      return context.prisma.post({ id })
-    },
-    themes: (parent, args, context) => {
-      return context.prisma.themes()
-    },
-    theme: (parent, { id }, context) => {
-      return context.prisma.theme({ id })
-    }
+    // feed: (parent, args, context) => {
+    //   return context.prisma.posts({ where: { published: true } })
+    // },
+    // drafts: (parent, args, context) => {
+    //   return context.prisma.posts({ where: { published: false } })
+    // },
+    // post: (parent, { id }, context) => {
+    //   return context.prisma.post({ id })
+    // },
+    // themes: (parent, args, context) => {
+    //   return context.prisma.themes()
+    // },
+    // theme: (parent, { id }, context) => {
+    //   return context.prisma.theme({ id })
+    // }
   },
   Mutation: {
-    createDraft(parent, { title, content }, context) {
-      return context.prisma.createPost({
-        title,
-        content
-      })
-    },
-    deletePost(parent, { id }, context) {
-      return context.prisma.deletePost({ id })
-    },
-    publish(parent, { id }, context) {
-      return context.prisma.updatePost({
-        where: { id },
-        data: { published: true }
-      })
-    }
+    // createDraft(parent, { title, content }, context) {
+    //   return context.prisma.createPost({
+    //     title,
+    //     content
+    //   })
+    // },
+    // deletePost(parent, { id }, context) {
+    //   return context.prisma.deletePost({ id })
+    // },
+    // publish(parent, { id }, context) {
+    //   return context.prisma.updatePost({
+    //     where: { id },
+    //     data: { published: true }
+    //   })
+    // }
   }
 }
