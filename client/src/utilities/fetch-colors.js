a46fcbdac6a47a321ab4b441a26551261c491626
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-06-12 13:47:49 -0700

    Feature/color fetch utility (#1)
    * basic fetch color example
    
    * add color suggestion to colormind api call
    
    * updated colormind fetch to accept and return hex
    
    * change return on fetch colors to object

---
+++ client/src/utilities/fetch-colors.js
@@ -0,0 +1,57 @@
+/*
+basic color fetch example need to change function signature to accept and return in the following format
+
+{
+    primaryLight,
+    accentLight,
+    accentBrand,
+    accentDark,
+    primaryDark
+  }
+
+*/
+
+import { rgbToHex, hexToRgb } from './rgbhex'
+
+export default function fetchColors(
+  {
+    primaryLight = 'N',
+    accentLight = 'N',
+    accentBrand = 'N',
+    accentDark = 'N',
+    primaryDark = 'N'
+  } = {
+    primaryLight: 'N',
+    accentLight: 'N',
+    accentBrand: 'N',
+    accentDark: 'N',
+    primaryDark: 'N'
+  }
+) {
+  const parsedInput = [
+    primaryLight,
+    accentLight,
+    accentBrand,
+    accentDark,
+    primaryDark
+  ].map(hex => hexToRgb(hex) || 'N')
+
+  return fetch(`http://colormind.io/api/`, {
+    method: 'POST',
+    body: JSON.stringify({
+      model: `ui`,
+      input: parsedInput
+    })
+  })
+    .then(response => response.json())
+    .then(({ result }) => {
+      const [
+        primaryLight,
+        accentLight,
+        accentBrand,
+        accentDark,
+        primaryDark
+      ] = result.map(([r, g, b]) => rgbToHex(r, g, b))
+      return { primaryLight, accentLight, accentBrand, accentDark, primaryDark }
+    })
+}
