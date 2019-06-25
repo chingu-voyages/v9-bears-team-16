55a0af33bcedddb41b3a21350d0ee724947f598d
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-06-25 11:16:13 -0700

    add create color pallet mutation

--- client/src/utilities/mutations.js
+++ client/src/utilities/mutations.js
@@ -1,17 +1,48 @@
 import { gql } from 'apollo-boost'
 import { Mutation } from 'react-apollo'
 
-export const CreateFont = ({ title, variant }) => {
-  return (
-    <Mutation mutation={CREATE_FONT}>{/* still need to finish */}</Mutation>
-  )
-}
+// export const CreateFont = ({ title, variant }) => {
+//   return (
+//     <Mutation mutation={CREATE_FONT}>{/* still need to finish */}</Mutation>
+//   )
+// }
+
 const CREATE_FONT = gql`
   mutation($title: String!, $variant: String!) {
     createFont(data: { title: $title, variant: $variant }) {
       id
       title
       variant
+    }
+  }
+`
+
+const CREATE_COLOR_PALLETE = gql`
+  mutation(
+    $title: String
+    $primaryLight: String!
+    $accentLight: String!
+    $accentBrand: String!
+    $accentDark: String!
+    $primaryDark: String!
+  ) {
+    createColorPallete(
+      data: {
+        title: $title
+        primaryLight: $primaryLight
+        accentLight: $accentLight
+        accentBrand: $accentBrand
+        accentDark: $accentDark
+        primaryDark: $primaryDark
+      }
+    ) {
+      id
+      title
+      primaryLight
+      accentLight
+      accentBrand
+      accentDark
+      primaryDark
     }
   }
 `
